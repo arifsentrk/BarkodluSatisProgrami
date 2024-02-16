8d89592bd40e42c9ad1632c1d2074a9ef2e057c5
arifsentrk <arifcansenturk@gmail.com>
2024-02-17 01:19:20 +0300

    Kayıtlı ürün listeye getirme(Bringing registered products to the list)
    Kayıtlı ürünlerin listeye getirilmesi için gerekli kodları yazdım.(I wrote the necessary codes to bring registered products to the list.)

--- SATISPROGRAMIBARKODLU/SATISPROGRAMIBARKODLU/Fsatis.Designer.cs
+++ SATISPROGRAMIBARKODLU/SATISPROGRAMIBARKODLU/Fsatis.Designer.cs
@@ -34,6 +34,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fsatis));
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -1080,6 +1081,7 @@
             this.gridsatislistesi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.gridsatislistesi.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.gridsatislistesi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gridsatislistesi.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
@@ -1107,10 +1109,14 @@
             this.gridsatislistesi.Name = "gridsatislistesi";
             this.gridsatislistesi.RowHeadersVisible = false;
             this.gridsatislistesi.RowHeadersWidth = 51;
-            this.gridsatislistesi.RowTemplate.Height = 24;
+            this.gridsatislistesi.RowTemplate.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(3);
+            this.gridsatislistesi.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.Silver;
+            this.gridsatislistesi.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.gridsatislistesi.RowTemplate.Height = 30;
             this.gridsatislistesi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.gridsatislistesi.Size = new System.Drawing.Size(635, 584);
             this.gridsatislistesi.TabIndex = 1;
+            this.gridsatislistesi.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridsatislistesi_CellContentClick);
             // 
             // splitContainer2
             // 
@@ -1593,6 +1599,7 @@
             // SIL
             // 
             this.SIL.HeaderText = "SİL";
+            this.SIL.Image = ((System.Drawing.Image)(resources.GetObject("SIL.Image")));
             this.SIL.MinimumWidth = 6;
             this.SIL.Name = "SIL";
             this.SIL.Resizable = System.Windows.Forms.DataGridViewTriState.True;
--- SATISPROGRAMIBARKODLU/SATISPROGRAMIBARKODLU/Fsatis.cs
+++ SATISPROGRAMIBARKODLU/SATISPROGRAMIBARKODLU/Fsatis.cs
@@ -21,10 +21,10 @@ namespace SATISPROGRAMIBARKODLU
 
         private void tbarkod_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)
             {
                 string barkod = tbarkod.Text.Trim();
-                if(barkod.Length <=2 ) 
+                if (barkod.Length <= 2)
                 {
                     tmiktar.Text = barkod;
                     tbarkod.Clear();
@@ -32,41 +32,41 @@ namespace SATISPROGRAMIBARKODLU
                 }
                 else
                 {
-                    if(db.Urun.Any(a=> a.Barkod==barkod))
+                    if (db.Urun.Any(a => a.Barkod == barkod))
                     {
-                        var urun = db.Urun.Where(a=> a.Barkod == barkod).FirstOrDefault();
-                        int satirsayisi = gridsatislistesi.Rows.Count;
-                        double miktar = Convert.ToDouble(tmiktar.Text);
-                        bool eklenmismi = false;
-                        if(satirsayisi> 0)
-                        {
-                            for (int i = 0; i < satirsayisi; i++)
-                            {
-                                if (gridsatislistesi.Rows[i].Cells["barkod"].Value.ToString()==barkod)
-                                {
-                                    gridsatislistesi.Rows[i].Cells["Miktar"].Value = miktar + Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Miktar"].Value);
-                                    gridsatislistesi.Rows[i].Cells["Toplam"].Value = Math.Round(Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Miktar"].Value) * Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Fiyat"].Value), 2);
-                                    eklenmismi |= true;
+                        var urun = db.Urun.Where(a => a.Barkod == barkod).FirstOrDefault();
+                        UrunGetirListeye(urun, barkod, Convert.ToDouble(tmiktar.Text));
 
-                                }
+                    }
+                    else
+                    {
+                        int onek = Convert.ToInt32(barkod.Substring(0, 2));
+                        if (db.Terazi.Any(a => a.TeraziOnEk == onek))
+                        {
+                            string teraziurunno = barkod.Substring(2, 5);
+                            if (db.Urun.Any(a => a.Barkod == teraziurunno))
+                            {
+                                var urunterazi = db.Urun.Where(a => a.Barkod == teraziurunno).FirstOrDefault();
+                                double miktarkg = Convert.ToDouble(barkod.Substring(7, 5)) / 1000;
+                                UrunGetirListeye(urunterazi, teraziurunno, miktarkg);
+
+                            }
+                            else
+                            {
+                                Console.Beep(900, 2000);
+                                MessageBox.Show("Kg Ürün Ekleme Sayfası");
+
                             }
                         }
-                        if( !eklenmismi)
+                        else
+
                         {
-                            gridsatislistesi.Rows.Add();
-                            gridsatislistesi.Rows[satirsayisi].Cells["Barkod"].Value = barkod;
-                            gridsatislistesi.Rows[satirsayisi].Cells["UrunAdi"].Value = urun.UrunAd;
-                            gridsatislistesi.Rows[satirsayisi].Cells["UrunGrup"].Value = urun.UrunGrup;
-                            gridsatislistesi.Rows[satirsayisi].Cells["BİRİM"].Value = urun.Birim;
-                            gridsatislistesi.Rows[satirsayisi].Cells["Fiyat"].Value = urun.SatisFiyati;
-                            gridsatislistesi.Rows[satirsayisi].Cells["Miktar"].Value = miktar;
-                            gridsatislistesi.Rows[satirsayisi].Cells["Toplam"].Value = Math.Round(miktar * (double)urun.SatisFiyati, 2);
-                            gridsatislistesi.Rows[satirsayisi].Cells["AlisFiyati"].Value = urun.AlisFiyati;
-                            gridsatislistesi.Rows[satirsayisi].Cells["KdvTutari"].Value = urun.KdvTutari;
+                            Console.Beep(900, 2000);
+                            MessageBox.Show("Normal Ürün Ekleme Sayfası");
 
                         }
                     }
-                   
+
 
                 }
                 gridsatislistesi.ClearSelection();
@@ -74,25 +74,67 @@ namespace SATISPROGRAMIBARKODLU
                 tbarkod.Focus();
 
             }
-            
+
 
         }
+
+        private void UrunGetirListeye(Urun urun, string barkod, double miktar)
+        {
+            int satirsayisi = gridsatislistesi.Rows.Count;
+            //double miktar = Convert.ToDouble(tmiktar.Text);
+            bool eklenmismi = false;
+            if (satirsayisi > 0)
+            {
+                for (int i = 0; i < satirsayisi; i++)
+                {
+                    if (gridsatislistesi.Rows[i].Cells["barkod"].Value.ToString() == barkod)
+                    {
+                        gridsatislistesi.Rows[i].Cells["Miktar"].Value = miktar + Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Miktar"].Value);
+                        gridsatislistesi.Rows[i].Cells["Toplam"].Value = Math.Round(Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Miktar"].Value) * Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Fiyat"].Value), 2);
+                        eklenmismi |= true;
+
+                    }
+                }
+            }
+            if (!eklenmismi)
+            {
+                gridsatislistesi.Rows.Add();
+                gridsatislistesi.Rows[satirsayisi].Cells["Barkod"].Value = barkod;
+                gridsatislistesi.Rows[satirsayisi].Cells["UrunAdi"].Value = urun.UrunAd;
+                gridsatislistesi.Rows[satirsayisi].Cells["UrunGrup"].Value = urun.UrunGrup;
+                gridsatislistesi.Rows[satirsayisi].Cells["BİRİM"].Value = urun.Birim;
+                gridsatislistesi.Rows[satirsayisi].Cells["Fiyat"].Value = urun.SatisFiyati;
+                gridsatislistesi.Rows[satirsayisi].Cells["Miktar"].Value = miktar;
+                gridsatislistesi.Rows[satirsayisi].Cells["Toplam"].Value = Math.Round(miktar * (double)urun.SatisFiyati, 2);
+                gridsatislistesi.Rows[satirsayisi].Cells["AlisFiyati"].Value = urun.AlisFiyati;
+                gridsatislistesi.Rows[satirsayisi].Cells["KdvTutari"].Value = urun.KdvTutari;
+
+            }
+        }
+
 
         private void GenelToplam()
         {
-            if(gridsatislistesi.Rows.Count > 0)
+            double toplam = 0;
+            for (int i = 0; i < gridsatislistesi.Rows.Count; i++)
             {
-                double toplam = 0;
-                for(int i = 0; i<gridsatislistesi.Rows.Count; i++)
-                {
-                    toplam += Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Toplam"].Value);
+                toplam += Convert.ToDouble(gridsatislistesi.Rows[i].Cells["Toplam"].Value);
 
-                }
-                tgeneltoplam.Text=toplam.ToString("C2");
-                tbarkod.Clear();
-                tbarkod.Focus();
             }
+            tgeneltoplam.Text = toplam.ToString("C2");
+            tbarkod.Clear();
+            tbarkod.Focus();
         }
-        
+
+        private void gridsatislistesi_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 9)
+
+                gridsatislistesi.Rows.Remove(gridsatislistesi.CurrentRow);
+            gridsatislistesi.ClearSelection();
+            GenelToplam();
+            tbarkod.Focus();
+
+        }
     }
 }
